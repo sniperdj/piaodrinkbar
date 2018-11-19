d0e481de096bdaf742e8b649d10b5a6e488b663d
郑楠 <sniper_zheng@163.com>
2018-11-19 21:43:46 +0800

    小朴的品质生活项目

---
+++ piaodrinkbar/src/business/home/pages/home.vue
@@ -0,0 +1,44 @@
+<template>
+	<el-container style="height: 500px; border: 1px solid #eee">
+		<el-header>
+			<el-menu mode="horizontal">
+				<el-menu-item index="0">首页</el-menu-item>
+				<el-submenu index="1">
+					<template slot="title">菜品介绍</template>
+					<el-menu-item index="1-0">啤酒下酒菜</el-menu-item>
+					<el-menu-item index="1-1">红酒下酒菜</el-menu-item>	
+					<el-menu-item index="1-2">韩式家常菜</el-menu-item>
+				</el-submenu>
+				<el-submenu index="2">
+					<template slot="title">酒类介绍</template>
+					<el-menu-item index="2-0">啤酒</el-menu-item>
+					<el-menu-item index="2-1">红酒</el-menu-item>	
+					<el-menu-item index="2-2">烧酒</el-menu-item>
+				</el-submenu>
+				<el-menu-item index="3">休闲游记</el-menu-item>
+				<el-menu-item index="4">关于</el-menu-item>
+			</el-menu>
+		</el-header>
+		<el-main>
+			{{ mains }}
+		</el-main>
+		<el-footer>
+			{{ footer }}
+		</el-footer>
+	</el-container>
+</template>
+
+<script>
+
+export default {
+	name:'Home',
+	data() {
+	    return {
+	    	mains: 'main area',
+	    	footer: 'footer area'
+	    }
+    
+  	}
+}
+
+</script>
---
+++ piaodrinkbar/src/business/home/pages/mock.js
@@ -0,0 +1,63 @@
+let homeMenu = {
+	"menuName":"首页",
+	"parentMenu":""
+}
+
+let eatMenu = {
+	"menuName":"菜品介绍",
+	"parentMenu":""
+}
+
+let beerEatMenu = {
+	"menuName":"啤酒下酒菜",
+	"parentMenu":eatMenu
+}
+let wineEatMenu = {
+	"menuName":"红酒下酒菜",
+	"parentMenu":eatMenu
+}
+let normalEatMenu = {
+	"menuName":"韩式家常菜",
+	"parentMenu":eatMenu
+}
+
+let drinkMenu = {
+	"menuName":"酒类介绍",
+	"parentMenu":""
+}
+let normalEatMenu = {
+	"menuName":"红酒",
+	"parentMenu":drinkMenu
+}
+let normalEatMenu = {
+	"menuName":"啤酒",
+	"parentMenu":drinkMenu
+}
+
+let travelMenu = {
+	"menuName":"休闲游记",
+	"parentMenu":""
+}
+
+let aboutMenu = {
+	"menuName":"关于",
+	"parentMenu":""
+}
+
+let menuBar = [
+	homeMenu,
+	eatMenu,
+	drinkMenu,
+	travelMenu,
+	aboutMenu
+]
+
+let newsList = ['标题标题0', 
+				'标题标题1', 
+				'标题标题2', 
+				'标题标题3', 
+				'标题标题4', 
+				'标题标题5', 
+				'标题标题6', 
+				'标题标题7',
+				'标题标题8']
