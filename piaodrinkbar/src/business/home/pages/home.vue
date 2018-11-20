9b5e66227f49ec6f8054ba5726e58482cc3b2e75
郑楠 <sniper_zheng@163.com>
2018-11-20 16:21:27 +0800

    更新菜单

--- piaodrinkbar/src/business/home/pages/home.vue
+++ piaodrinkbar/src/business/home/pages/home.vue
@@ -2,11 +2,18 @@
 	<div id="piaoMenu">
 		<el-container style="height: 500px; border: 1px solid #eee">
 		<el-header>
-			{{ header }}
-			<el-menu mode="horizontal">
-				<el-menu-item v-for="menu in menuList">
-					{{ menu.menuTitle }}
-				</el-menu-item>
+			<el-menu mode="horizontal" class="el-menu-vertical-demo" @select="didSelectMenu">
+				<template v-for="(menu, index) in menuList">
+					<el-submenu :index="index+''" v-if="menu.hasSubMenu">
+						<template slot="title">{{ menu.menuTitle }}</template>
+	                    <el-menu-item :index="subMe.menuIndex" v-for="(subMe, index) in menu.subMenu">
+							{{ subMe.menuTitle }}
+						</el-menu-item>
+					</el-submenu>
+					<el-menu-item :index="menu.menuIndex" v-if="!menu.hasSubMenu">
+						{{ menu.menuTitle }}
+					</el-menu-item>
+				</template>
 			</el-menu>
 		</el-header>
 		<el-main>
@@ -24,33 +31,41 @@
 export default {
 	data() {
 	    return {
-	    	header: 'head area',
 	    	mains: 'main area',
 	    	footer: 'footer area',
 	    	menuList: [
 						{
 							menuTitle:"首页",
+							hasSubMenu: false,
+							menuIndex: 0,
 							subMenu:[]
 						},
 						{
 							menuTitle:"菜品介绍",
+							hasSubMenu: true,
+							menuIndex: 1,
 							subMenu:[
 								{
 									menuTitle:"啤酒下酒菜",
+									menuIndex: 0,
 									subMenu:[]
 								},
 								{
 									menuTitle:"红酒下酒菜",
+									menuIndex: 1,
 									subMenu:[]
 								},
 								{
 									menuTitle:"韩式家常菜",
+									menuIndex: 2,
 									subMenu:[]
 								}
 							]
 						},
 						{
 							menuTitle:"酒类介绍",
+							hasSubMenu: true,
+							menuIndex: 2,
 							subMenu:[
 								{
 									menuTitle:"啤酒",
@@ -68,16 +83,25 @@ export default {
 						},
 						{
 							menuTitle:"休闲游记",
+							hasSubMenu: false,
+							menuIndex: 3,
 							subMenu:[]
 						},
 						{
 							menuTitle:"关于",
+							hasSubMenu: false,
+							menuIndex: 4,
 							subMenu:[]
 						}
 					]
 	    }
     
-  	}
+  	},
+  	methods: {
+      didSelectMenu(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
 }
 
 </script>
--- piaodrinkbar/src/business/home/pages/mock.js
+++ piaodrinkbar/src/business/home/pages/mock.js
@@ -1,58 +1,72 @@
-let newsList = ['标题标题0', 
-				'标题标题1', 
-				'标题标题2', 
-				'标题标题3', 
-				'标题标题4', 
-				'标题标题5', 
-				'标题标题6', 
-				'标题标题7',
-				'标题标题8']
+let newsList = [
+					{newsTitle: "标题0", updateTime:"2018-11-20"},
+					{newsTitle: "标题1", updateTime:"2018-11-19"},
+					{newsTitle: "标题2", updateTime:"2018-11-15"},
+					{newsTitle: "标题3", updateTime:"2018-10-14"},
+					{newsTitle: "标题4", updateTime:"2018-07-04"}
+				]
 
 let menu = [
-	{
-		menuTitle:"首页",
-		subMenu:[]
-	},
-	{
-		menuTitle:"菜品介绍",
-		subMenu:[
-			{
-				menuTitle:"啤酒下酒菜"
-				subMenu:[]
-			},
-			{
-				menuTitle:"红酒下酒菜"
-				subMenu:[]
-			},
-			{
-				menuTitle:"韩式家常菜"
-				subMenu:[]
-			}
-		]
-	},
-	{
-		menuTitle:"酒类介绍",
-		subMenu:[
-			{
-				menuTitle:"啤酒"
-				subMenu:[]
-			},
-			{
-				menuTitle:"红酒"
-				subMenu:[]
-			},
-			{
-				menuTitle:"韩式烧酒"
-				subMenu:[]
-			}
-		]
-	},
-	{
-		menuTitle:"休闲游记",
-		subMenu:[]
-	},
-	{
-		menuTitle:"关于",
-		subMenu:[]
-	}
-]
+				{
+					menuTitle:"首页",
+					hasSubMenu: false,
+					menuIndex: "0",
+					subMenu:[]
+				},
+				{
+					menuTitle:"菜品介绍",
+					hasSubMenu: true,
+					menuIndex: "1",
+					subMenu:[
+						{
+							menuTitle:"啤酒下酒菜",
+							menuIndex: "0",
+							subMenu:[]
+						},
+						{
+							menuTitle:"红酒下酒菜",
+							menuIndex: "1",
+							subMenu:[]
+						},
+						{
+							menuTitle:"韩式家常菜",
+							menuIndex: "2",
+							subMenu:[]
+						}
+					]
+				},
+				{
+					menuTitle:"酒类介绍",
+					hasSubMenu: true,
+					menuIndex: "2",
+					subMenu:[
+						{
+							menuTitle:"啤酒",
+							menuIndex: "0",
+							subMenu:[]
+						},
+						{
+							menuTitle:"红酒",
+							menuIndex: "1",
+							subMenu:[]
+						},
+						{
+							menuTitle:"韩式烧酒",
+							menuIndex: "2",
+							subMenu:[]
+						}
+					]
+				},
+				{
+					menuTitle:"休闲游记",
+					hasSubMenu: false,
+					menuIndex: "3",
+					subMenu:[]
+				},
+				{
+					menuTitle:"关于",
+					hasSubMenu: false,
+					menuIndex: "4",
+					subMenu:[]
+				}
+			]
